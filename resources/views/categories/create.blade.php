e3a1f7827476c664017423fd2780abbef512dda7
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-05-09 20:43:35 -0300

    adicionar categoria css

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 @section('content')
-<h2>Criar categoria</h2>
+<h2 class="text-center">Criar categoria</h2>
 
-<form action="{{ route('categories.store') }}" class="p-3 bg-white" method="POST">
+<form action="{{ route('categories.store') }}" class="p-3 col-md-6 offset-3" method="POST">
     @csrf
-    <div class="form-group">
-        <label for="name">Nome</label>
-        <input type="text" class="form-control" name="name" placeholder="Nome da categoria">
-    </div>
 
-    <button type="submit" class="btn btn-success">Criar</button>
+    <div class="card">
+        <div class="card-body">
+            <div class="label-float">
+                <input name="name" type="text" placeholder=" " />
+                <label>Nome</label>
+            </div>
+
+            <button type="submit" class="btn btn-success mt-4 float-right">Criar</button>
+        </div>
+    </div>
 </form>
 
 @endsection
