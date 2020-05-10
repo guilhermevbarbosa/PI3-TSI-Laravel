83184ee6de0458b9f7b2292219daa518d6e3256a
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-05-10 13:14:26 -0300

    soft delete de produto e categoria adicionado

--- app/Product.php
+++ app/Product.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    // protected $fillable = ['name', 'description', 'image', 'price', 'discount', 'stock', 'category_id'];
+    use SoftDeletes;
     protected $fillable = ['name', 'description', 'image', 'price', 'discount', 'stock'];
+    // protected $fillable = ['name', 'description', 'image', 'price', 'discount', 'stock', 'category_id'];
 }
