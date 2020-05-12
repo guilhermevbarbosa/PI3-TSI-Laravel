f5d4fca4e60d92b3a07b1d56b10f7f6bf0c6dfd4
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-05-12 16:06:11 -0300

    swiper

--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    @yield('css')
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -24,8 +25,63 @@
 <body>
     <div id="app">
 
+        <nav class="navbar store fixed-top navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    GVPM Store
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        <main class="py-4 container">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+                    <!-- Left Side Of Navbar -->
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a href="{{route('user.edit')}}" class="dropdown-item">Perfil</a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item">{{ __('Logout') }}</button>
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                        <!-- Authentication Links -->
+                    </ul>
+                    <!-- Right Side Of Navbar -->
+                </div>
+        </nav>
+
+        <main>
             @if(session()->has('success'))
             <div class="alert alert-success">
                 <i class="far fa-thumbs-up"></i> {{ session()->get('success') }}
--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.store')
 
+@section('css')
+<link href="{{ asset('css/swiper.css') }}" rel="stylesheet">
+@endsection
+
+@section('javascript')
+<script src="{{ asset('js/swiper.js') }}"></script>
+<script src="{{ asset('js/home-swipe.js') }}"></script>
+@endsection
+
 @section('content')
-{{ $products }}
+<div class="swiper-container">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="https://via.placeholder.com/1080x400/FEFEFE">
+        </div>
+
+        <div class="swiper-slide">
+            <img src="https://via.placeholder.com/1080x400/dc4e4e">
+        </div>
+
+        <div class="swiper-slide">
+            <img src="https://via.placeholder.com/1080x400/179636">
+        </div>
+    </div>
+
+    <!-- Pagination -->
+    <div class="swiper-pagination"></div>
+    <!-- Pagination -->
+
+    <!-- Navigation buttons -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    <!-- Navigation buttons -->
+</div>
+
+{{-- {{ $products }} --}}
 @endsection
