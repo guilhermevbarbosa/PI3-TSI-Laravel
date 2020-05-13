ce0db97c359dfbce4145bd0774fd7173e1ebbce1
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-05-13 11:52:56 -0300

    WIP categ/tags

---
+++ resources/views/store/search.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.store')
+
+@section('content')
+<section class="container pad-cont">
+    <div class="row">
+        <div class="mx-auto col-10 text-center">
+            <h2 class="text-uppercase"> {{ $title }} </h2>
+        </div>
+
+        <div class="row">
+            @foreach ($products as $product)
+            <div class="mx-auto col-sm-10 col-md-6 col-lg-3">
+                <img src="{{ asset('storage/'.$product->image) }}" alt="{{ $product->name }}" class="img-fluid">
+                <span>{{$product->name}}</span>
+            </div>
+            @endforeach
+        </div>
+
+        <div class="d-flex justify-content-center mt-4">
+            {{ $products->links() }}
+        </div>
+    </div>
+</section>
+@endsection
