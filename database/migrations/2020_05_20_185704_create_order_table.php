ef792072d550ed7fa71c3008ca3e1b6ed05f477f
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-05-20 16:02:58 -0300

    criada as migrations

--- database/migrations/2020_05_20_185704_create_order_table.php
+++ database/migrations/2020_05_20_185704_create_order_table.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCartProductTable extends Migration
+class CreateOrderTable extends Migration
 {
     public function up()
     {
-        Schema::create('cart_product', function (Blueprint $table) {
-            $table->integer('cart_id');
-            $table->integer('product_id');
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('cart_product');
+        Schema::dropIfExists('order');
     }
 }
--- database/migrations/2020_05_20_185810_create_order_product_table.php
+++ database/migrations/2020_05_20_185810_create_order_product_table.php
@@ -4,20 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCartsTable extends Migration
+class CreateOrderProductTable extends Migration
 {
     public function up()
     {
-        Schema::create('carts', function (Blueprint $table) {
-            $table->id();
-            $table->integer('user_id');
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->integer('order_id');
             $table->integer('product_id');
+            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('order_product');
     }
 }
