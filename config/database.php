6961ce75c71cbb7204e7dc9502f1788b824aeec3
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-05-21 22:45:34 -0300

    heroku db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
 
+$DATABASE_URL=parse_url('postgres://tjpeijhilbhxle:6b2ce3919fc8466799cfdebac82954c4a3d7d60acdb11926edcd18f2a30a7f51@ec2-34-198-243-120.compute-1.amazonaws.com:5432/dbuuqlb2jvn1en');
 use Illuminate\Support\Str;
 
 return [
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,12 +67,12 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
+            'charset' => 'utf8mb4',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
