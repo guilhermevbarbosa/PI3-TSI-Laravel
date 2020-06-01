d22b56ec0abbc8cace60a12e1b1717362d00497a
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-05-31 21:46:43 -0300

    Remove do estoque ao comprar

--- app/Http/Controllers/CartsController.php
+++ app/Http/Controllers/CartsController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Cart;
@@ -15,19 +14,22 @@ class CartsController extends Controller
         $cart = $user->cart;
         $totalPrice = 0;
 
-        if($cart->count() > 0){
-            foreach ($cart as $product) {
+        if ($cart->count() > 0)
+        {
+            foreach ($cart as $product)
+            {
                 $forProd = Product::withTrashed()->find($product->product_id);
 
                 $products[] = $forProd;
             }
-        }else{
+        }
+        else
+        {
             $products = null;
             $totalPrice = null;
         }
 
-        return view('carts.index')
-        ->with('prod', $products);
+        return view('carts.index')->with('prod', $products);
     }
 
     public function store(Product $product)
@@ -39,13 +41,14 @@ class CartsController extends Controller
         ->where('product_id', $product->id)
         ->first();
 
-        if($consulta){
+        if ($consulta)
+        {
             $actualAmount = $consulta->amount;
 
-            $consulta->update([
-                'amount' => $actualAmount + 1
-            ]);
-        }else{
+            $consulta->update(['amount' => $actualAmount + 1]);
+        }
+        else
+        {
             Cart::create([
                 'user_id' => $user,
                 'product_id' => $product->id,
@@ -61,18 +64,17 @@ class CartsController extends Controller
     {
         $user = auth()->user()->id;
 
-        $consulta = Cart::all()
-        ->where('user_id', $user )
-        ->where('product_id', $product->id)
-        ->first();
+        $consulta = Cart::all()->where('user_id', $user)->where('product_id', $product->id)
+            ->first();
 
-        if($consulta->amount > 1){
+        if ($consulta->amount > 1)
+        {
             $actualAmount = $consulta->amount;
 
-            $consulta->update([
-                'amount' => $actualAmount - 1
-            ]);
-        }else {
+            $consulta->update(['amount' => $actualAmount - 1]);
+        }
+        else
+        {
             $consulta->delete();
         }
 
@@ -80,26 +82,28 @@ class CartsController extends Controller
         return redirect()->back();
     }
 
-    public function checkout(){
+    public function checkout()
+    {
         $userId = auth()->user()->id;
 
-        // VERIFICA SE TEM ESTOQUE DOS PRODUTOS DO CARRINHO
         $orderItens = Cart::all()->where('user_id', $userId);
 
-        foreach($orderItens as $prod){
+        // VERIFICA SE TEM ESTOQUE DOS PRODUTOS DO CARRINHO
+        foreach ($orderItens as $prod)
+        {
             $actualProd = Product::find($prod->product_id);
+            $stock = $actualProd->stock;
 
-            if($actualProd->stock == 0){
-                session()->flash('error', 'O produto '.$actualProd->name. ' não tem mais estoque, remova-o para continuar a compra');
+            if ($prod->amount > $stock)
+            {
+                session()->flash('error', 'O produto ' . $actualProd->name . ' não tem estoque, só há ' . $stock . ' no estoque.');
                 return redirect()->back();
             }
         }
         // VERIFICA SE TEM ESTOQUE DOS PRODUTOS DO CARRINHO
 
         // CRIA O PEDIDO NA TABELA ORDER
-        $order = Order::create([
-            'user_id' => $userId
-        ]);
+        $order = Order::create(['user_id' => $userId]);
 
         // PESQUISA NA TABELA DO CARRINHO OS PRODUTOS E INSERE NA TABELA ORDER_PRODUCT
         $this->searchAndInsertInOrderProductTable($userId, $order->id);
@@ -111,40 +115,39 @@ class CartsController extends Controller
         return redirect('/orders');
     }
 
-    private function searchAndInsertInOrderProductTable(int $userId, int $orderId){
-      // PEGA OS PRODUTOS DO CARRINHO DO USUÁRIO
-      $orderItens = Cart::all()->where('user_id', $userId);
+    private function searchAndInsertInOrderProductTable(int $userId, int $orderId)
+    {
+        // PEGA OS PRODUTOS DO CARRINHO DO USUÁRIO
+        $orderItens = Cart::all()->where('user_id', $userId);
 
-      foreach($orderItens as $prod){
-          $actualProd = Product::find($prod->product_id);
+        foreach ($orderItens as $prod)
+        {
+            $actualProd = Product::find($prod->product_id);
 
-        // CRIA O REGISTRO NA TABELA ORDER_PRODUCT
-          OrderProduct::create([
-              'order_id' => $orderId,
-              'product_id' => $actualProd->id,
-              'price' => $actualProd->discountPriceOnlyVal() * $prod->amount,
-              'amount' => $prod->amount
-          ]);
+            // CRIA O REGISTRO NA TABELA ORDER_PRODUCT
+            OrderProduct::create([
+                'order_id' => $orderId,
+                'product_id' => $actualProd->id,
+                'price' => $actualProd->discountPriceOnlyVal() * $prod->amount,
+                'amount' => $prod->amount
+            ]);
 
-        // REMOVE DO ESTOQUE O PRODUTO
-        $stock = $actualProd->stock;
-
-        if($stock > 0){
+            // REMOVE DO ESTOQUE O PRODUTO
+            $stock = $actualProd->stock;
             $stock -= $prod->amount;
-        }
 
-        $actualProd->update([
-            'stock' => $stock
-        ]);
-        // REMOVE DO ESTOQUE O PRODUTO
-      }
+            $actualProd->update(['stock' => $stock]);
+            // REMOVE DO ESTOQUE O PRODUTO
+        }
     }
 
-    private function deleteDataAfterCheckout(int $userId){
+    private function deleteDataAfterCheckout(int $userId)
+    {
         // REMOVE OS PRODUTOS DO CARRINHO DO CLIENTE
         $prodsToRemove = Cart::all()->where('user_id', $userId);
 
-        foreach($prodsToRemove as $actualProd){
+        foreach ($prodsToRemove as $actualProd)
+        {
             $actualProd->delete();
         }
     }
